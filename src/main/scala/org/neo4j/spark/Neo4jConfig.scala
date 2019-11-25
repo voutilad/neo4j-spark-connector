e042deea9a1835825d9292b4555dae25dc3d88f4
echohlne <52202080+echohlne@users.noreply.github.com>
2019-11-25 17:39:54 +0800

    Update Neo4jConfig.scala
    In NEO4J, if the parameter dbms.connector.bolt. Tls_level is set to REQUIRED, when we connect NEO4J with neo4j-spark connector, the connection will report an error:Connection to the database terminated. This can happen due to network instabilities, or due to restarts of the database.So We add a parameter to control how the Config instance can be created,If the encryptionStatus variable is false, use the withoutEncryption() method to initialize the config instance,otherwise use the withEncryption() to initialize it.

--- src/main/scala/org/neo4j/spark/Neo4jConfig.scala
+++ src/main/scala/org/neo4j/spark/Neo4jConfig.scala
@@ -4,30 +4,38 @@ import org.apache.spark.SparkConf
 import org.neo4j.driver.v1.{Driver, AuthTokens, Config, GraphDatabase}
 
 /**
-  * @author mh
-  * @since 02.03.16
-  */
-case class Neo4jConfig(val url: String, val user: String = "neo4j", val password: Option[String] = None) {
+ * @author mh
+ * @since 02.03.16
+ */
+case class Neo4jConfig(url: String, user: String = "neo4j", password: Option[String] = None, encryptionStatus: Boolean) {
+  //If the encryptionStatus variable is false, use the withEncryption() method to initialize the config instance,
+  // otherwise use the withoutEncryption() to initialize it.
+  private def createBoltConfig() = if (encryptionStatus) Config.build().withEncryption().toConfig else Config.build().withoutEncryption().toConfig
 
-  def boltConfig() = Config.build.withoutEncryption().toConfig
-
-  def driver(config: Neo4jConfig) : Driver = config.password match {
-    case Some(pwd) => GraphDatabase.driver(config.url, AuthTokens.basic(config.user, pwd), boltConfig())
-    case _ => GraphDatabase.driver(config.url, boltConfig())
+  def driver(config: Neo4jConfig): Driver = config.password match {
+    case Some(pwd) => GraphDatabase.driver(config.url, AuthTokens.basic(config.user, pwd), createBoltConfig())
+    case _ => GraphDatabase.driver(config.url, createBoltConfig())
   }
 
-  def driver() : Driver = driver(this)
+  def driver(): Driver = driver(this)
 
-  def driver(url: String): Driver = GraphDatabase.driver(url, boltConfig())
+  def driver(url: String): Driver = GraphDatabase.driver(url, createBoltConfig())
 
 }
 
 object Neo4jConfig {
-  val prefix = "spark.neo4j.bolt."
+  // List of currently supported parameters
+  private val URL = "spark.neo4j.bolt.url"
+  private val USER = "spark.neo4j.bolt.user"
+  private val PASSWORD = "spark.neo4j.bolt.password"
+  private val ENCRYPTION_STATUS = "spark.neo4j.bolt.encryption.status"
+
   def apply(sparkConf: SparkConf): Neo4jConfig = {
-    val url = sparkConf.get(prefix + "url", "bolt://localhost")
-    val user = sparkConf.get(prefix + "user", "neo4j")
-    val password: Option[String] = sparkConf.getOption(prefix + "password")
-    Neo4jConfig(url, user, password)
+    val url = sparkConf.get(URL, "bolt://localhost")
+    val user = sparkConf.get(USER, "neo4j")
+    val password: Option[String] = sparkConf.getOption(PASSWORD)
+    // default value is false
+    val encryptionStatus = sparkConf.getBoolean(ENCRYPTION_STATUS, defaultValue = false)
+    Neo4jConfig(url, user, password, encryptionStatus)
   }
 }
