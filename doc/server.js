1a3ab5f87d2c2a32941d4fe0b0f0d603670b15e3
Mauro Roiter <mauro.roiter@larus-ba.it>
2020-07-17 15:21:14 +0200

    Migrated documentation to Antora

---
+++ doc/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const app = express()
+app.use(express.static('./build/site'))
+
+app.get('/', (req, res) => res.redirect('/docs/1.0.0'))
+
+app.listen(8000, () => console.log('📘 http://localhost:8000'))
