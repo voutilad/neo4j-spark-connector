350e9389374c70e0f3356bdb914d62975d09fd4d
Andrea Santurbano <santand@gmail.com>
2020-09-21 16:07:32 +0200

    removed unused method call

--- src/main/scala/org/neo4j/spark/util/Validations.scala
+++ src/main/scala/org/neo4j/spark/util/Validations.scala
@@ -36,7 +36,6 @@ object Validations {
             s"${Neo4jOptions.RELATIONSHIP_SOURCE_LABELS} is required when Save Mode is Overwrite")
           ValidationUtil.isNotEmpty(neo4jOptions.relationshipMetadata.target.labels,
             s"${Neo4jOptions.RELATIONSHIP_TARGET_LABELS} is required when Save Mode is Overwrite")
-          schemaService.structForRelationship()
         }
       }
     } finally {
