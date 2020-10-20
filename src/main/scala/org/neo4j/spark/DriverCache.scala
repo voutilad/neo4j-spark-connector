398a8610bacd5fb101c9d10f9eb0b9fc52e5df0d
utnaf <davide.fantuzzi@larus-ba.it>
2020-10-20 12:09:01 +0200

    refactored

--- src/main/scala/org/neo4j/spark/DriverCache.scala
+++ src/main/scala/org/neo4j/spark/DriverCache.scala
@@ -8,16 +8,16 @@ import org.neo4j.spark.DriverCache.{cache, jobIdCache}
 import org.neo4j.spark.util.Neo4jUtil
 
 object DriverCache {
-  private val cache: ConcurrentHashMap[Neo4jDriverOptions, Neo4jDriverWrapper] = new ConcurrentHashMap[Neo4jDriverOptions, Neo4jDriverWrapper]
+  private val cache: ConcurrentHashMap[Neo4jDriverOptions, Driver] = new ConcurrentHashMap[Neo4jDriverOptions, Driver]
   private val jobIdCache = Collections.newSetFromMap[String](new ConcurrentHashMap[String, java.lang.Boolean]())
 }
 
 class DriverCache(private val options: Neo4jDriverOptions, private val jobId: String) extends Serializable with AutoCloseable {
-  def getOrCreate(): Neo4jDriverWrapper = {
+  def getOrCreate(): Driver = {
     this.synchronized {
       jobIdCache.add(jobId)
-      cache.computeIfAbsent(options, new function.Function[Neo4jDriverOptions, Neo4jDriverWrapper] {
-        override def apply(t: Neo4jDriverOptions): Neo4jDriverWrapper = new Neo4jDriverWrapper(GraphDatabase.driver(t.url, t.toNeo4jAuth, t.toDriverConfig))
+      cache.computeIfAbsent(options, new function.Function[Neo4jDriverOptions, Driver] {
+        override def apply(t: Neo4jDriverOptions): Driver = GraphDatabase.driver(t.url, t.toNeo4jAuth, t.toDriverConfig)
       })
     }
   }
@@ -26,21 +26,11 @@ class DriverCache(private val options: Neo4jDriverOptions, private val jobId: St
     this.synchronized {
       jobIdCache.remove(jobId)
       if(jobIdCache.isEmpty) {
-        val driverWrapper = cache.remove(options)
-        if (driverWrapper != null) {
-          Neo4jUtil.closeSafety(driverWrapper.getDriver)
+        val driver = cache.remove(options)
+        if (driver != null) {
+          Neo4jUtil.closeSafety(driver)
         }
       }
     }
   }
-}
-
-class Neo4jDriverWrapper(driver: Driver) {
-  def session(sessionConfig: SessionConfig): Session = {
-    val session = driver.session(sessionConfig)
-    session.run("EXPLAIN MATCH (n) RETURN count(n)").consume()
-    session
-  }
-
-  def getDriver: Driver = driver
 }
