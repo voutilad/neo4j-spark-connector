f542df220422ae1d7358f716e20a2c35158c289c
Adam Cowley <adam.cowley@neotechnology.com>
2020-11-10 13:05:49 +0000

    Fix redirect for local preview (#247)

--- doc/server.js
+++ doc/server.js
@@ -3,6 +3,6 @@ const express = require('express')
 const app = express()
 app.use(express.static('./build/site'))
 
-app.get('/', (req, res) => res.redirect('/neo4j-spark-docs/1.0.0'))
+app.get('/', (req, res) => res.redirect('/spark'))
 
 app.listen(8000, () => console.log('📘 http://localhost:8000'))
