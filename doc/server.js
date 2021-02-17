0f58d2c0aae7601a85bdcaceb71ab2085cf75150
Guillaume Grossetie <g.grossetie@gmail.com>
2021-02-17 16:58:41 +0100

    Fix and check links with hyperlink (#271)
    * Fix and check links with hyperlink
    
    * Add CI jobs
    
    * Trigger if documentation changed
    
    * Build on fork
    
    * Remove temporary build on fork repo
    
    * Remove comment
    
    * Remove console.log

--- doc/server.js
+++ doc/server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 app.use(express.static('./build/site'))
 
-app.get('/', (req, res) => res.redirect('/spark'))
+app.use('/static/assets', express.static('./build/site/developer/_'))
+
+app.get('/', (req, res) => res.redirect('/developer/spark'))
 
 app.listen(8000, () => console.log('📘 http://localhost:8000'))
---
+++ doc/tasks/lint-links.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const hyperlink = require('hyperlink')
+const TapRender = require('@munter/tap-render');
+
+const root = path.join(__dirname, '..')
+
+;(async () => {
+  const tapRender = new TapRender()
+  tapRender.pipe(process.stdout)
+  try {
+    const skipPatterns = [
+      // initial redirect
+      'load index.html',
+      'load docs/index.html',
+      // google fonts
+      'load https://fonts.googleapis.com/',
+      // static resources
+      'load static/assets',
+      // externals links
+      // /
+      'load try-neo4j',
+      // /developer
+      'load developer',
+      // /labs
+      'load labs',
+      // /docs
+      'load docs',
+      // rate limit on twitter.com (will return 400 code if quota exceeded)
+      'external-check https://twitter.com/neo4j',
+      // workaround: not sure why the following links are not resolved properly by hyperlink :/
+      'load build/site/developer/spark/quickstart/reading',
+      'load build/site/developer/spark/quickstart/writing'
+    ]
+    const skipFilter = (report) => {
+      return Object.values(report).some((value) => {
+          return skipPatterns.some((pattern) => String(value).includes(pattern))
+        }
+      )
+    };
+    await hyperlink({
+        root,
+        inputUrls: [`build/site/developer/spark/index.html`],
+        skipFilter: skipFilter,
+        recursive: true,
+      },
+      tapRender
+    )
+  } catch (err) {
+    console.log(err.stack);
+    process.exit(1);
+  }
+  const results = tapRender.close();
+  process.exit(results.fail ? 1 : 0);
+})()
